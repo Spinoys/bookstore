5806d9f259d055c55fddd82700f507e81cfd94ba
N0rth <omur.balsever@gmail.com>
2014-11-23 16:37:40 +0100

    Example code of updating table

--- index.php
+++ index.php
@@ -8,3 +8,12 @@ $password = "password";
 $dbname = "ourDB";
 
 //Create connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+//Check connection and return error message if connection fails
+if (!$conn) {
+  die("Woops, the connection to the database failed: " .mysqli_connect_error());
+}
+
+$sql = "INSERT INTO Leverans (isbn, titel, author, fprice, deliverydate, numberdelivered)
+VALUES ('12345', 'Book Titel', 'Författare', '100', 'datum', '5')";
