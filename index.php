81655faf60efe2f168b712161d62eb4754e7aa18
benaking <benjisweden@hotmail.com>
2014-11-25 18:54:22 +0100

    Lägger till html mall som kan byggas vidare på samt jquery fallback

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
-//Firing up PHP
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Bookstore</title>
+    <meta name="description" content="Awesome bookstore">
+    <link rel="stylesheet" href="css/meyer-reset.css">
+
+    <!-- main.css-->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <!-- App starts-->
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+
+    <main>
+        <p>Hello world im ready to rock!</p>
+
+
+
+    </main>
+
+    <footer>
+
+    </footer>
+    <!-- App ends-->
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <!-- jQuery fallback om jQuery inte kan laddas om google ligger nere eller är bannat i ett land kör vi jQuery från vår server måste vara i vanilla js! -->
+    <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+    <!-- Main.js here-->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+
+
 <?php
+//Firing up PHP
 
 //Adding local database info
 $servername = 127.0.0.1;
@@ -25,3 +68,4 @@ if (mysqli_query($con, $sql)) {
 
 mysqli_close($conn);
 }
+?>
