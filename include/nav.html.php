1efe319965ce3bd358e925c947cbe673a15b7f5f
Mangemannen66 <magnus@md-produktion.se>
2014-11-27 19:58:04 +0100

    Gjort en include mapp och däri placerat nav.html.php filen

---
+++ include/nav.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Navigation</title>
+</head>
+<body>
+	    <ul>    
+            <li><a href="index.php">Start</a></li>
+            <li><a href="leverans.php">Inleverans</a></li>  
+            <li><a href="index.php">Försäljning</a></li>
+            <li><a href="index.php">Rapporter</a></li>
+        </ul>
+</body>
+</html>
