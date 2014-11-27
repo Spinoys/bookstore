5e245bc969c41fb4cba6ebeacef493f2589020f2
Mangemannen66 <magnus@md-produktion.se>
2014-11-27 14:11:55 +0100

    Ändrat i nav index2 till leverans.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <nav>
         <ul>    
             <li><a href="index.php">Start</a></li>
-            <li><a href="index2.php">Inleverans</a></li>  
+            <li><a href="leverans.php">Inleverans</a></li>  
             <li><a href="index.php">Försäljning</a></li>
             <li><a href="index.php">Rapporter</a></li>
         </ul>
