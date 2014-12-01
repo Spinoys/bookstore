c275b5bd1f2e9e2f677cfc1e76d4138bda866e09
Spinoys <akersfalt@gmail.com>
2014-12-01 12:21:10 +0100

    försäljningsformulär påbörjat 2014-12-02

---
+++ forsalj.php
@@ -0,0 +1,41 @@
+forsalj.php<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Bookstore</title>
+    <meta name="description" content="Awesome bookstore">
+    <link rel="stylesheet" href="css/meyer-reset.css">
+    <!-- main.css-->
+    <link rel="stylesheet" href="css/main.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <!-- jQuery fallback om jQuery inte kan laddas om google ligger nere eller är bannat i ett land kör vi jQuery från vår server måste vara i vanilla js! -->
+    <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+    <!-- Main.js here-->
+    <!--<script src="js/main.js"></script>-->
+</head>
+
+<body>
+    <!-- App starts-->
+    <header>
+        <nav>
+            <?php include 'include/nav.html.php'; ?>
+        </nav>
+    </header>
+
+    <main>
+        <h3>Försäljningsformulär</h3>
+        
+
+
+    </main>
+
+    <footer>
+    <p>FooterInfo</p>
+    </footer>
+    <!-- App ends-->
+
+
+</body>
+
+</html>
