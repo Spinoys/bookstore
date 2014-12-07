771a9a5cbd6ad8ac637063c8b7c8b885bc1583d8
N0rth <omur.balsever@gmail.com>
2014-12-07 11:42:07 +0100

    Fetch all the data necessary from table with the Current Stock

--- getbook.php
+++ getbook.php
@@ -10,3 +10,22 @@
  if(!$db){
   die('Database selection failed '.mysql_error());
  }
+<!-- Select all from correct table -->
+
+ $sql = 'SELECT * FROM current_stock_extended';
+
+ $result = mysql_query($sql,$conn);
+
+ <!-- Fetch the data from table -->
+
+  $data = array();
+ while($row = mysql_fetch_array($result)){
+  $row_data = array(
+   'isbn' => $row['isbn'],
+   'current_stock' => $row['current_stock'],
+    'author_first_name' => $row['author_first_name'],
+    'author_first_name' => $row['author_first_name'],
+    'description' => $row['description']
+   );
+  array_push($data, $row_data);
+ }
