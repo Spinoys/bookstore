a48af8c4d237baa7bf82f12d8ecbece10b8b2bc8
Spinoys <akersfalt@gmail.com>
2014-12-07 18:26:54 +0100

    en bit på väg

---
+++ forsaljning.php
@@ -0,0 +1,25 @@
+<?php
+//Includes the magic connector!
+include 'pdo_helper.php';
+
+//if we have a post
+if (isset($_POST['isbn'], $_POST['quantity'])) {
+	$isbn = strip_tags($_POST['isbn']);
+	$quantity = strip_tags($_POST['quantity']);
+	
+	//book sold
+	//register sale
+	$dbh->query("INSERT INTO sales (isbn, sales_quantity, selling_date)
+  	VALUES('$isbn', '$quantity', CURRENT_TIMESTAMP)");
+  	
+
+	$result = $dbh->jsonQuery("SELECT * FROM sales ORDER BY delivery_date DESC LIMIT 3");
+// Eka ut JSON:en till din AJAX-drivna webb:
+	//echo ($result);
+	echo json_encode($result);
+} else {
+	echo json_encode('Something went wrong!');
+}
+
+
+?>
