277929342beffcf1953f3867848553ecfe349f6b
N0rth <omur.balsever@gmail.com>
2014-12-07 11:36:02 +0100

    Connect to database

--- getbook.php
+++ getbook.php
@@ -3,3 +3,10 @@
  if(!$conn){
   die('Mysql connection error '.mysql_error());
  }
+
+ <!-- Connect to the correct database -->
+
+ $db = mysql_select_db('bookstore',$conn);
+ if(!$db){
+  die('Database selection failed '.mysql_error());
+ }
