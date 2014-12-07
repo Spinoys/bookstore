83fc03167ef8408c2a09d2e5ef78d96c8ed7da1d
Spinoys <akersfalt@gmail.com>
2014-12-07 12:25:24 +0100

    börjat jobba med försäljningens sida

---
+++ js/moduler/forsaljning-modul.js
@@ -0,0 +1,40 @@
+//forsaljning-modul ajax
+    $('form').submit(function(event) {
+
+        //Form datan
+        var formData = {
+            'isbn'          : $('input[name=isbn]').val(),
+            'quantity'      : $('input[name=quantity]').val()
+        };
+
+        $.ajax({
+            beforeSend: function() {
+             $("#forsaljning_resultat_ajax").show().html('<img src="img/ajaxsnurra.gif">Laddar...');
+                },
+            type        : 'POST',
+            url         : 'forsalj.php',
+            data        : formData, // our data object
+            dataType    : 'json',
+            success: function(data){
+                //Getting data to console
+                console.log(data);
+                
+                //clear input fields
+                $('.form-input').find(':input').each(function() {
+                    switch(this.type) {
+                        case 'text':
+                            $(this).val('');
+                            break;
+                    }
+                });
+
+                $("#forsaljning_resultat_ajax").show().html('Försäljningen är registrerad!');
+
+            },
+             error: function(xhr, status, error) {
+            $("#forsaljning_resultat_ajax").show().html('Kunde inte registrera försäljningen.');
+             }
+        });
+        // försäkrar oss om att den inte kan skickas som en vanlig php sida som behöver laddas om.
+        event.preventDefault();
+    });
